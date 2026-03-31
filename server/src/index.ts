ad796dd70d622d6bb961c2635eed033da08bb30c
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2026-03-31 10:32:26 -0400

    Created S3 specific functions that enable file upload to timeline buckets

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,14 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 import { ContentType } from "./generated/prisma/enums";
-
 import express from "express";
+import multer from "multer";
 import prisma from "./db";
+import { getPresignedUrl, uploadFile } from "./s3";
 
 const app = express();
 const port = 3001;
+const upload = multer({ storage: multer.memoryStorage() });
 
 //note: just to allow the app to parse json
 app.use(express.json());
@@ -66,18 +68,25 @@ app.get("/api/memories/:date", async (req, res) => {
 
 // ------------------------------ ALL MEMORY CARD ROUTES -----------------------------
 //TODO: POST create a memory card given a lot of things
-app.post("/api/cards", async (req, res) => {
+app.post("/api/cards", upload.single("file"), async (req, res) => {
   const request = req.body;
+
+  let content = request.content;
+
+  if (req.file) {
+    content = await uploadFile(req.file);
+  }
+
   const memory_card = await prisma.memoryCard.create({
     data: {
       type: request.type as ContentType,
-      content: request.content,
+      content: content,
       date: new Date(request.date),
-      position_x: request.position_x,
-      position_y: request.position_y,
-      z_index: request.z_index,
-      width: request.width,
-      height: request.height,
+      position_x: parseInt(request.position_x),
+      position_y: parseInt(request.position_y),
+      z_index: parseInt(request.z_index),
+      width: parseInt(request.width),
+      height: parseInt(request.height),
 
       user_id: request.user_id,
       memory_id: request.memory_id,
@@ -114,6 +123,29 @@ app.patch("/api/cards/size/:id", async (req, res) => {
   });
   res.json(memory_card);
 });
+
+//TODO: test for multer
+app.post("/api/upload", upload.single("file"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file provided" });
+  }
+
+  const key = await uploadFile(req.file);
+  const url = await getPresignedUrl(key);
+
+  res.json({
+    key: key, // this is saved in the db
+    url: url, // presigned url to use for 1 hour
+  });
+});
+
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   console.log(req.file);
+//   res.json({
+//     message: "File received",
+//     file: req.file,
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
