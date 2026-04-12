d6f61f4f0faf2a582dc2b2470319a598a5a08891
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2026-04-12 19:21:14 -0400

    updated url google callback

--- server/src/auth/auth.ts
+++ server/src/auth/auth.ts
@@ -8,9 +8,8 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       callbackURL:
-        process.env.NODE_ENV === "production"
-          ? "https://timeline-production-600c.up.railway.app/api/auth/google/callback"
-          : "http://localhost:3001/api/auth/google/callback",
+        process.env.GOOGLE_CALLBACK_URL ||
+        "http://localhost:3001/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
