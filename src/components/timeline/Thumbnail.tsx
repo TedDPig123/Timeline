e257da06e6709118caeb3989ea01945738ad0845
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2026-05-03 17:14:17 -0400

    fixed thumbnail offset

--- src/components/timeline/Thumbnail.tsx
+++ src/components/timeline/Thumbnail.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 
 export default function Thumbnail({
   text,
-  image = "",
+  image = null,
   date,
   dayLabel = null,
 }: {
@@ -13,6 +13,9 @@ export default function Thumbnail({
 }) {
   const [lineClamp, setLineClamp] = useState(2);
   const containerRef = useRef<HTMLDivElement | null>(null);
+
+  // const thumbnailRef = useRef<HTMLDivElement | null>(null);
+  // const [currThumbnailDim, setCurrThumbnailDim] = useState([1, 1]);
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -27,13 +30,25 @@ export default function Thumbnail({
       }
     };
 
-    const resizeObserver = new ResizeObserver(updateLineClamp);
-    resizeObserver.observe(containerRef.current);
+    // const updateDim = () => {
+    //   if (thumbnailRef.current) {
+    //     const width = thumbnailRef.current.clientWidth;
+    //     const height = thumbnailRef.current.clientHeight;
+    //     setCurrThumbnailDim([width, height]);
+    //   }
+    // };
 
+    const resizeObserverClamp = new ResizeObserver(updateLineClamp);
+    resizeObserverClamp.observe(containerRef.current);
     updateLineClamp();
 
+    // const resizeObserverDim = new ResizeObserver(updateDim);
+    // resizeObserverDim.observe(thumbnailRef.current);
+    // updateDim();
+
     return () => {
-      resizeObserver.disconnect();
+      resizeObserverClamp.disconnect();
+      // resizeObserverDim.disconnect();
     };
   }, []);
 
@@ -42,32 +57,41 @@ export default function Thumbnail({
       className="thumbnail relative z-10 flex flex-col items-center justify-start rounded-[28px] border-[3px] border-black bg-white hover:scale-[1.01] hover:shadow-md"
       data-date={date}
     >
-      <span className="absolute top-[-15px] z-20 bg-white pl-1 pr-1 font-bold">
+      <span className="absolute top-[-15px] z-20 bg-white px-2 font-bold">
         {dayLabel && <span className="mr-2">{dayLabel}</span>}
         {date}
       </span>
-      <div className="w-full overflow-hidden rounded-[16px] border-[3px] border-white bg-white">
-        {image && (
-          <img
-            className="mb-1 h-auto w-full rounded-[16px]"
-            src={image}
-            alt="thumbnail image"
-          />
-        )}
-        {text && (
-          <div ref={containerRef} className="mt-1 h-[100%] w-[100%] flex-1">
-            <p
-              className="w-full overflow-hidden text-ellipsis text-[14px]"
-              style={{
-                display: "-webkit-box",
-                WebkitLineClamp: lineClamp,
-                WebkitBoxOrient: "vertical",
-              }}
-            >
-              {text}
-            </p>
-          </div>
-        )}
+      <div className="flex h-full w-full flex-col items-center overflow-hidden p-2">
+        <>
+          {image && (
+            <div className="w-full overflow-hidden rounded-[16px] border-[3px] border-white bg-white">
+              <img
+                className="h-auto w-full rounded-[12px] object-cover"
+                src={image}
+                alt="thumbnail"
+              />
+            </div>
+          )}
+          {text && (
+            <div ref={containerRef} className="mt-1 h-full w-full flex-1">
+              <p
+                className="w-full overflow-hidden text-ellipsis text-[14px]"
+                style={{
+                  display: "-webkit-box",
+                  WebkitLineClamp: lineClamp,
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                {text}
+              </p>
+            </div>
+          )}
+          {!image && !text && (
+            <div className="flex h-full w-full items-center justify-center">
+              <span className="text-2xl text-gray-300">• • •</span>
+            </div>
+          )}
+        </>
       </div>
     </span>
   );
