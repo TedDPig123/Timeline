1aadac488b00b77c59a7d97221f47fe31eb7f4bb
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2026-05-05 01:01:40 -0400

    added new themes: earth and coffee

--- src/context/theme.tsx
+++ src/context/theme.tsx
@@ -1,6 +1,4 @@
-import { theme } from "./context";
-
-export const themes: { light: theme; dark: theme } = {
+export const themes = {
   light: {
     name: "light",
     primaryColor: "#ffffff", //background
@@ -15,6 +13,22 @@ export const themes: { light: theme; dark: theme } = {
     secondaryColor: "#D4D4D4", //text and borders
     tertiaryColor: "#374151", //settings and signout buttons
     highlightColor: "#6b7280",
+    isDark: true,
+  },
+  coffee: {
+    name: "coffee",
+    primaryColor: "#4B2E2B", //background
+    secondaryColor: "#FFF8F0", //text and borders
+    tertiaryColor: "#374151", //settings and signout buttons
+    highlightColor: "#C08552", //hover colors
+    isDark: true,
+  },
+  earth: {
+    name: "earth",
+    primaryColor: "#546B41", //background
+    secondaryColor: "#FFF8EC", //text and borders
+    tertiaryColor: "#374151", //settings and signout buttons
+    highlightColor: "#99AD7A", //hover colors
     isDark: true,
   },
 };
